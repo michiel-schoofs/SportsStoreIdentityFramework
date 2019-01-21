df3b4742af55849b04265f497045a9c4328f33f4
Michiel Schoofs <michiel.schoofs@student.hogent.be>
2019-01-21 16:45:58 +0100

    Rework Index of Products Enums

---
+++ IdentityFrameworkSportsstore/Helpers/EnumHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace IdentityFrameworkSportsstore.Helpers {
+    public static class EnumHelpers {
+        public static SelectList ToSelectList<TEnum>() {
+            var values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>()
+                    .Select(e => new { Id = e, Name = e.GetDisplayName() });
+            return new SelectList(values, "Id", "Name");
+        }
+
+        public static string GetDisplayName<TEnum>(this TEnum enumValue) {
+            return typeof(TEnum).GetMember(enumValue.ToString())[0]
+                           .GetCustomAttribute<DisplayAttribute>()?
+                           .Name ?? enumValue.ToString();
+        }
+    }
+}
--- IdentityFrameworkSportsstore/Models/Domain/Availability.cs
+++ IdentityFrameworkSportsstore/Models/Domain/Availability.cs
@@ -1,8 +1,13 @@
-﻿namespace IdentityFrameworkSportsstore.Models.Domain {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityFrameworkSportsstore.Models.Domain {
     public enum Availability {
         #region Enum
+        [Display(Name ="Shop only")]
         ShopOnly,
+        [Display(Name ="Online only")]
         OnlineOnly,
+        [Display(Name ="Shop and online")]
         ShopAndOnline 
         #endregion
     }
